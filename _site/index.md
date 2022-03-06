983cd696e53bd10fcd1c9a4cb14651e86a441d11
Jeremy <jermybz01@gmail.com>
2022-03-06 14:28:36 -0600

    initial commit

---
+++ _site/index.md
@@ -0,0 +1,36 @@
+## RESUME
+
+### EDUCATION
+
+**Candidate for Associates of Information Technology**
+
+University of Belize, Belmopan, Cayo, Aug. 2020 - present
+
+**Relevant Assignments:**
+**Web Development Labs,** Web Development, Aug. - November 2021
+
+- Semantically coded the HTML of the web pages
+- Added styling to web pages with CSS
+- Added interactivity to web pages with Javascript
+
+**Relevant Courses:** Web Development, Data Structures, Networking Fundamentals, Systems Analysis and
+Design, Hardware Fundamentals
+
+
+### WORK EXPERIENCE
+
+**Web Developer,** Honduras, Mar. 2022 - present
+
+- Build client websites 
+  - Static HTML website 
+  - Work with basic FB apis
+  - Work with basic Git commands and UI
+- Install google analytics and ensure it works
+- Test websites and applications, including links and adding and editing data
+
+
+### HOBBIES
+
+- Drawing
+- Watching videos
+- Browsing the internet
---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function download() {
+    var container = document.getElementById('print-area');
+    
+    var element = document.getElementById('page');
+    var button = document.getElementsByClassName('btn');
+
+    function addClass() {
+        container.classList.add('page');
+        button.forEach(element => {
+            element.classList.add('hide')
+        });
+    }
+
+
+    var opt = {
+      margin:       0,
+      filename:     'resume.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 4, y: 0, scrollY: 0, removeContainer: false, dpi: 300},
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' },
+      //pagebreak: { mode: 'avoid-all' }
+    };
+    
+    // New Promise-based usage:
+    function print() {
+        html2pdf().set(opt).from(element).save();
+    }
+
+
+    function removeClass() {
+        container.classList.remove('page');
+        button.forEach(element => {
+            element.classList.remove('hide')
+        });
+    }
+
+    addClass();
+    setTimeout(print, 2000);
+    setTimeout(removeClass, 5000);
+}
+
